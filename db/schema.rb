560fbca5583e8ca2b1b010a19b085eb382c73aeb
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-14 04:18:58 +0000

    [Add]rails migration, products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_034720) do
+ActiveRecord::Schema.define(version: 2022_01_14_041001) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,17 @@ ActiveRecord::Schema.define(version: 2022_01_14_034720) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "image_id"
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ship_addresses", force: :cascade do |t|
