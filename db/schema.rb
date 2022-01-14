18cf4cde57d93722d4fa9633d1c917ceb44894fc
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-14 04:22:48 +0000

    [Add]genre migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_041001) do
+ActiveRecord::Schema.define(version: 2022_01_14_042021) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2022_01_14_041001) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
