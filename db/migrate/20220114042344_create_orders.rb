60229885c08c7a55467c1b6b25f77349d62517d6
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-14 04:27:57 +0000

    [Add]order model

---
+++ db/migrate/20220114042344_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+
+      t.integer :customer_id, null: false
+      t.integer :payment_method, null: false, default: 0
+      t.string :post_code,     null: false
+      t.string :address,       null: false
+      t.string :address_owner, null: false
+      t.integer :shiping_fee, null: false
+      t.integer :charge, null: false
+      t.integer :status, null: false, default: 0
+      t.timestamps
+    end
+  end
+end
