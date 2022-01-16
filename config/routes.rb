05456b76f0eaa28016b971d3be459ec70d854cda
wilas0o0o <03mcu30@gmail.com>
2022-01-16 05:07:08 +0000

    [Fix]Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
-  get "about" => "homes#about", as: "about"
 
-  resources :products, only: [:index, :show] do
-    collection do
-      get "search" => "products#search"
-    end
-  end
 
   # 顧客用ルーティング
   devise_for :customers,skip: [:passwords,], controllers: {
@@ -16,27 +9,35 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
-    resource :customers, only: [:show, :edit, :update] do
+    resource :customers, only: [:edit, :update] do
       collection do
+        get 'my_page' => "customers#show"
         get "unsubscribe" => "customers#unsubscribe"
         patch "out" => "customers#out"
       end
     end
 
-    resources :cart_items, only: [:index, :create, :update, :destroy] do
+    root to: "homes#top"
+    get "about" => "homes#about", as: "about"
+
+    resources :products, only: [:index, :show] do
       collection do
-        delete "destroy_all" => "cart_items#destroy_all"
+        get "search" => "products#search"
+      end
+    end
+
+    resources :cart_products, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete "destroy_all" => "cart_products#destroy_all"
       end
     end
 
     resources :orders, only: [:new, :create, :index, :show] do
       collection do
-        get "log" => "orders#log"
+        get "confirm" => "orders#confirm"
         get "thanx" => "orders#thanx"
       end
     end
-
-    resource :order_products, only: [:create]
 
     resources :ship_addresses, only: [:index, :create, :edit, :update, :destroy]
   end
@@ -48,7 +49,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
 
