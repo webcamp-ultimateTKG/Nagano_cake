e0f75dd7fcfceb8079ba57d57ec4d8032dc82e7d
wilas0o0o <03mcu30@gmail.com>
2022-01-16 04:56:43 +0000

    [Fix]controller

--- test/controllers/public/products_controller_test.rb
+++ test/controllers/public/products_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Public::CartItemsControllerTest < ActionDispatch::IntegrationTest
+class Public::ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
