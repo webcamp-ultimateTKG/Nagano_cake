3d521869871154b27220f957fefab000cdb976ff
wilas0o0o <03mcu30@gmail.com>
2022-01-16 04:53:07 +0000

    [Delete]NoFile migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_042914) do
+ActiveRecord::Schema.define(version: 2022_01_16_044429) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2022_01_14_042914) do
   end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cart_products", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "product_id", null: false
     t.integer "quantity", default: 1, null: false
