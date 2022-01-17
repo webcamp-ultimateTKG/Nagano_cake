12ebd164dafb756f7e19f6b800bd816d1c7d7cdb
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-17 15:33:44 +0000

    [Add] customers/validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,15 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :email, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
   def full_name
     self.last_name + " " + self.first_name
   end
