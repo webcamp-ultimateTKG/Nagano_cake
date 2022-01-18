d161a871fbd074e401b3f4e86f442268be73f3dc
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-18 08:04:37 +0000

    [Add]admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ProductsController < ApplicationController
+  before_action :move_to_signed_in
+
+  def index
+    @products = Product.page(params[:page]).reverse_order
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+    def move_to_signed_in
+      unless admin_signed_in?
+        #管理者ログインしていない場合はtopページに行く
+        redirect_to root_path
+      end
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :genre
+
+  def add_tax_price
+      (self.price * 1.10).round
+  end
 end
