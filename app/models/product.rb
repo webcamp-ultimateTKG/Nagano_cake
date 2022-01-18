111c801a73d37e435c1c6c988b60d3afb8a5a047
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-18 08:08:21 +0000

    [Add] validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :cart_products
   has_many :order_products
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :introduction, presence: true
   validates :price, presence: true, numericality: {only_integer: true}
   validates :genre_id, presence: true
