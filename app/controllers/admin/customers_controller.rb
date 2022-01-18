d24c3a619f02142b41378720fb1b1ba4f3b3c923
wilas0o0o <03mcu30@gmail.com>
2022-01-18 05:26:00 +0000

    [Start]admin/customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,15 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
