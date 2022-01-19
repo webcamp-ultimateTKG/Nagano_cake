18de3063877920c5869b75e8eaf8b7de96c00af3
wilas0o0o <03mcu30@gmail.com>
2022-01-19 07:37:05 +0000

    [Finish]cart_create

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -8,18 +8,17 @@ class Public::CartProductsController < ApplicationController
   def create
     @cart_product = current_customer.cart_products.new(cart_product_params)
     @cart_product.product_id = params[:cart_product][:product_id]
-    mycart = CartProduct.where(customer_id: current_customer.id)
+    current_cart_product = current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id])
     #選択した商品が既にカート内に存在する場合
-    if mycart.find_by(params[:product_id])
-      @cart_product.quantity += params[:cart_product][:quantity].to_i
-      @cart_product.update(cart_product_params)
+    if current_cart_product.present?
+      current_cart_product.quantity += params[:cart_product][:quantity].to_i
+      current_cart_product.save
       redirect_to cart_products_path
     #選択した商品がカート内に存在しない場合
     else
       @cart_product.save
       redirect_to cart_products_path
     end
-
   end
 
   def update
