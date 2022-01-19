71d2c4bd1b37513211068aaf9ddd27f8476a8c76
wilas0o0o <03mcu30@gmail.com>
2022-01-19 02:02:58 +0000

    [Start]wlias-cart

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,2 +1,31 @@
 class Public::CartProductsController < ApplicationController
+  before_action :authenticate_customer!
+  
+  def index
+  end
+  
+  def create
+    @cart_product = CartProduct.new(cart_product_params)
+    @cart_product.customer_id = current_customer.id
+    if @cart_product.save
+      redirect_to cart_products_path
+    else
+      render "products/show"
+    end
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def destroy_all
+  end
+  
+  private
+    def cart_product_params
+      params.require(:cart_prroduct).permit(:product_id, :quantity)
+    end
+    
 end
