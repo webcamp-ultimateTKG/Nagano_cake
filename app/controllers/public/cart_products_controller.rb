10a314600a90d3a4acb4a7dade40858330217012
wilas0o0o <03mcu30@gmail.com>
2022-01-19 03:41:40 +0000

    [Middle]

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -6,7 +6,15 @@ class Public::CartProductsController < ApplicationController
   
   def create
     @cart_product = CartProduct.new(cart_product_params)
+    @product = Product.find(cart_product_params[:product_id])
     @cart_product.customer_id = current_customer.id
+    #カートに選択した商品が既に存在するかどうか
+    if CartProduct.find_by(product_id: @product)
+      @cart_product.quantity += cart_product_params[:product_id].to_i
+    else
+      @cart_product.build(product_id: @product)
+    end
+    #カートに商品を保存する
     if @cart_product.save
       redirect_to cart_products_path
     else
--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,3 +1,10 @@
 class CartProduct < ApplicationRecord
   belongs_to :product
+  
+  #カートに商品を追加
+  def add_product 
+    product = Product.find_by(product_id: product_id) || Product.build(product_id: product_id)
+    product.quantity += quantity.to_i
+    product.save
+  end
 end
