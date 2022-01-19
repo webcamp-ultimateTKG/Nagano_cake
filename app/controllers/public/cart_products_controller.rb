4057802a3febb6431b8c89fcd0766888c97dd621
wilas0o0o <03mcu30@gmail.com>
2022-01-19 06:19:46 +0000

    [Middle]

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -6,28 +6,18 @@ class Public::CartProductsController < ApplicationController
   end
 
   def create
-    @cart_product = CartProduct.new(cart_product_params)
-    @cart_product.customer_id = current_customer.id
+    @cart_product = current_customer.cart_products.new(cart_product_params)
     @cart_product.product_id = params[:cart_product][:product_id]
     mycart = CartProduct.where(customer_id: current_customer.id)
     #選択した商品が既にカート内に存在する場合
     if mycart.find_by(params[:product_id])
-      @cart_product.quantity += params[:quantity].to_i
-      # @cart_product.id = CartProduct.find(id: mycart.id)
-      if @cart_product.update(cart_product_params)
-        redirect_to cart_products_path
-      else
-        @product = Product.find(cart_product_params[:product_id])
-        render "public/products/show"
-      end
+      @cart_product.quantity += params[:cart_product][:quantity].to_i
+      @cart_product.update(cart_product_params)
+      redirect_to cart_products_path
     #選択した商品がカート内に存在しない場合
     else
-      if @cart_product.save
-        redirect_to cart_products_path
-      else
-        @product = Product.find(cart_product_params[:product_id])
-        render "public/products/show"
-      end
+      @cart_product.save
+      redirect_to cart_products_path
     end
 
   end
