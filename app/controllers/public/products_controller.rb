f4f085d5732c9802c965dda92118ea47b814f531
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-19 05:57:18 +0000

    [Add]public/products/index

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,13 @@
 class Public::ProductsController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @products = Product.where(genre_id:@genre.id).page(params[:page]).per(6)
+    else
+    @products = Product.page(params[:page]).per(6)
+    end
+
+  end
 end
