9b747ffbf0a00e0452173076eb176d5dd793985b
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-20 07:54:02 +0000

    [Add]order/new

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,11 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, dependent: :destroy
 
+  validates :customer_id, presence: :true
+  validates :post_code, presence: :true
+  validates :address, presence: :true
+  validates :owner, presence: :true
+  validates :shipping_fee, presence: :true
+  validates :charge, presence: :true
+
 end
