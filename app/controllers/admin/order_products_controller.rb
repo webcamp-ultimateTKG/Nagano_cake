77813f8983f42f07403807543e8d23d23d5f17ef
wilas0o0o <03mcu30@gmail.com>
2022-01-20 03:52:00 +0000

    [Start]admin/order_show

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,2 +1,11 @@
 class Admin::OrderProductsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def create
+  end
+  
+  private
+  
+    def order_product_params
+    end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,20 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def search
+  end
+  
+  private
+  
+    def order_params
+    end
 end
