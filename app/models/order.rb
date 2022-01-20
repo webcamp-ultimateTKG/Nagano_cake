18f833a3cb47970f155d4ba623eed832e2596b23
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-20 07:15:53 +0000

    [Add] admin/orders/index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, through: :orders, source: :product, dependent: :destroy
+
+  def total_quantity
+    self.order_products.all.sum(:quantity)
+  end
 end
