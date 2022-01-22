bea75d856be7f6c0a879156452a20eccd08172f3
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-22 05:29:45 +0000

    [Add] selectbox_style

--- app/models/ship_address.rb
+++ app/models/ship_address.rb
@@ -5,13 +5,8 @@ class ShipAddress < ApplicationRecord
   validates :address, presence: true
   validates :owner, presence: true, format: { with: VALID_NAME_REGEX }
 
-  def with_owner_full_address
+  def full_address
     '〒' + post_code.to_s.insert(3, "-") + ' ' + address + ' ' + owner
   end
-
-  def full_address
-    '〒' + post_code.to_s.insert(3, "-") + ' ' + address
-  end
-
 
 end
